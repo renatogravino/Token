fac6ecc01d97e377d7f86f50dafa99f13c130d42
Renato Gravino Neto <renato.gravino.neto@gmail.com>
2022-12-26 14:53:16 -0300

    feat: novo token RGN

---
+++ README.md
@@ -0,0 +1,10 @@
+# Token de exemplo para o curso da dio
+
+Bloco que foi publicado
+ https://goerli.etherscan.io/tx/0x28fdc29433c05ef62fd24aa49e010f62f7ca9b33ff7858251ba23487a03c70c5
+
+
+ Endereço do contrato 0xA6bA231BcC52eb470276FC5553d03ec00311B416
+
+Token
+https://goerli.etherscan.io/token/0xa6ba231bcc52eb470276fc5553d03ec00311b416?a=0x8b6b088d9e15e9e35b3c75ef88e4cb9ab8e8156c
---
+++ token.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RGNToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("GRN Coin", "GRN") {
+        _mint(msg.sender, 200000000 * 10**decimals());
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 2;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
